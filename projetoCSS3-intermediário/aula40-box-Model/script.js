056005142882079b53161367355c147335b78508
Bruno Barreto da Cunha Holanda <114125291+BBarretoCH@users.noreply.github.com>
2023-01-06 21:44:22 -0300

    conteúdos curso-html-css3-udemy

---
+++ projetoCSS3-intermediário/aula40-box-Model/script.js
@@ -0,0 +1,9 @@
+function clicar() {
+    var a = document.getElementsByClassName('conteudo')[0]
+    a.innerHTML = 'Esse é meu novo conteúdo'   
+}
+
+function clicar1() {
+    var b = document.getElementsByClassName ('conteudo')[1]
+    b.innerHTML = 'Este é novamente o meu novo conteúdo' 
+}
